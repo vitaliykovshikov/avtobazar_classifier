3ea027d02f6656484fd69f3dca0cee23b7e435f2
Yura Revutskiy <dr.bleedjent@gmail.com>
2015-01-14 13:46:57 +0200

    Add setup py

---
+++ README.rst
@@ -0,0 +1,18 @@
+=====
+Classifier Avtobazar App
+=====
+
+Classifier is Django basic app for classifier table
+
+Quick start
+-----------
+
+1. Add "classifier" to INSTALLED_APPS:
+  INSTALLED_APPS = {
+    ...
+    'classifier'
+  }
+
+22. Run `python manage.py syncdb` to create myblog's models.
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='avtobazar_classifier',
+    version='0.0.1',
+    packages=['classifier', 'classifier.management', 'classifier.management.commands', 'classifier.migrations'],
+    url='',
+    license='',
+    author='Dr.bleedjent',
+    author_email='dr.bleedjent',
+    description=''
+)
